dae26801652fadab0bf654a92db0ac7e8bd8d497
Ryan Emberling <itsalaidbacklife@gmail.com>
2019-06-02 13:23:52 -0400

    created navbar, home, and research pages. Made dummy pages for team and contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResearchComponent } from './research/research.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ResearchComponent,
+    TeamComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
